739a585b0c17ea1eeca2cca1d41c0219a5833441
lacatoire <la.catoire@gmail.com>
2026-02-27 15:17:11 +0100

    [Webhook] Allow configuring server headers and signing algo via YAML

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -3394,6 +3394,14 @@ class FrameworkExtension extends Extension
 
         $jsonBodyConfigurator = $container->getDefinition('webhook.body_configurator.json');
         $jsonBodyConfigurator->replaceArgument(0, new Reference($serializerEnabled ? 'webhook.payload_serializer.serializer' : 'webhook.payload_serializer.json'));
+
+        $container->getDefinition('webhook.headers_configurator')
+            ->replaceArgument(0, $config['server']['event_header_name'])
+            ->replaceArgument(1, $config['server']['id_header_name']);
+
+        $container->getDefinition('webhook.signer')
+            ->replaceArgument(0, $config['server']['algorithm'])
+            ->replaceArgument(1, $config['server']['signature_header_name']);
     }
 
     private function registerRemoteEventConfiguration(PhpFileLoader $loader): void
